c8ab6db1eaa329b54a4240ef437c8ee257355a63
A Taylor <112668339+ATaylorAerospace@users.noreply.github.com>
2023-11-05 01:06:31 -0700

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,98 @@
+```python
+import torch
+from torch import optim
+from torch.utils.data import DataLoader
+from transformers import AdamW, get_linear_schedule_with_warmup
+from transformers import BertTokenizer, BertForSequenceClassification, BertConfig
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+
+# Define the training and validation datasets
+train_dataset = ...  # Add your training dataset here
+test_dataset = ...  # Add your test dataset here
+
+# Define the tokenizer and model
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
+
+# Define the training parameters
+batch_size = 2  # Adjust the batch size as needed
+num_epochs = 3  # Adjust the number of training epochs as needed
+learning_rate = 2e-5  # Adjust the learning rate as needed
+
+# Define the training and validation datasets
+train_dataset = ...  # Add your training dataset here (replace the ellipses)
+test_dataset = ...  # Add your test dataset here (replace the ellipses)
+
+# Define the training parameters
+batch_size = 2  # Adjust the batch size as needed, for example: batch_size = 8
+num_epochs = 3  # Adjust the number of training epochs as needed, for example: num_epochs = 10
+learning_rate = 2e-5  # Adjust the learning rate as needed, for example: learning_rate = 5e-5
+
+# Define the validation metrics
+val_metric = 'f1'  # Change to 'accuracy', 'recall', or 'precision' for different evaluation metrics
+
+# Define the training optimizer
+optimizer = AdamW(model.parameters(), lr=learning_rate)
+total_steps = len(train_dataset) * num_epochs // batch_size  # Calculate the total number of training steps
+
+# Define the learning rate scheduler
+scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=total_steps)
+
+# Define the training and validation functions
+def train(model, optimizer, scheduler, train_dataset, tokenizer):
+    model.train()
+    for batch in train_dataset:
+        optimizer.zero_grad()
+        input_ids, input_mask, segment_ids, label = batch
+        outputs = model(input_ids=input_ids, token_type_ids=segment_ids, attention_mask=input_mask, labels=label)
+        loss = outputs.loss
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+def validate(model, val_dataset, tokenizer):
+    model.eval()
+    predictions = []
+    actual_labels = []
+    for batch in val_dataset:
+        input_ids, input_mask, segment_ids, label = batch
+        with torch.no_grad():
+            outputs = model(input_ids=input_ids, token_type_ids=segment_ids, attention_mask=input_mask)
+            predicted_label = torch.argmax(outputs.logits, dim=1)
+            predictions.append(predicted_label.item())
+            actual_labels.append(label)
+    predictions = torch.cat(predictions, dim=0)
+    actual_labels = torch.cat(actual_labels, dim=0)
+    accuracy = accuracy_score(predictions, actual_labels)
+    f1 = f1_score(predictions, actual_labels, average='macro')
+    recall = recall_score(predictions, actual_labels, average='macro')
+    precision = precision_score(predictions, actual_labels, average='macro')
+    return accuracy, f1, recall, precision
+
+# Define the training and validation functions
+train_function = train
+validate_function = validate
+
+# Fine-tune the model on the training dataset
+train_dataset = ...  # Add your training dataset here (replace the ellipses)
+test_dataset = ...  # Add your test dataset here (replace the ellipses)
+
+# Fine-tune the model on the training dataset
+train(model, optimizer, scheduler, train_dataset, tokenizer)
+
+# Evaluate the model on the test dataset
+accuracy, f1, recall, precision = validate(model, test_dataset, tokenizer)
+print('Test accuracy:', accuracy)
+print('Test f1:', f1)
+print('Test recall:', recall)
+print('Test precision:', precision)
+
+# Save the trained model
+model_path = 'trained_model.pt'
+torch.save(model.state_dict(), model_path)
+print('Trained model saved to:', model_path)
+```
+
+In the above code, the training and validation datasets are defined as `train_dataset` and `val_dataset`, respectively. The tokenizer and model are defined as `tokenizer` and `model`, respectively. The training parameters are defined as `batch_size`, `num_epochs`, and `learning_rate`. The validation metrics are defined as `val_metric`, which can be set to 'accuracy', 'f1', 'recall', or 'precision' to evaluate the model on the validation dataset. The training and validation functions are defined as `train` and `validate`, respectively. The fine-tuning of the model on the training dataset is performed using the `train` function, and the evaluation of the model on the validation dataset is performed using the `validate` function. The trained model is saved using the `torch.save` function.
 
